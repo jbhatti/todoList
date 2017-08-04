7fe03fb2e07d3dfbcaf93341497543cfe65c1f06
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-08-03 21:44:14 -0400

    Initial Commit

---
+++ List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  List
+//
+//  Created by Jaison Bhatti on 2017-08-03.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ List/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  List
+//
+//  Created by Jaison Bhatti on 2017-08-03.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
