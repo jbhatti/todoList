3b512a19f14457b03e7ae379e88dd0e1687d3948
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-08-24 12:37:53 -0400

    todo list

---
+++ List/ListTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  ListTableViewCell.h
+//  List
+//
+//  Created by Jaison Bhatti on 2017-08-03.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+@end
---
+++ List/ListTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.m
+//  List
+//
+//  Created by Jaison Bhatti on 2017-08-03.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+#import "ListTableViewCell.h"
+
+@implementation ListTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
